b8227064e0ec9e27d53dbead19c3ecb4297b9716
Khánh28- <nguyendanhnamkhanh06@gmail.com>
2024-12-09 01:40:39 +0700

    Update bài tạp1ss7.c

--- bài tạp1ss7.c
+++ bài tạp1ss7.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int main(){
-    int length; // do dai cua mang 
-    printf("moi ban nhap do dai cua mang");
-    scanf("%d" ,&length);
-
-    int array[length];
-    for(int i = 0 ;i < length ; i++){
-        printf("array(%d) = %d\n ,i , array[i]");
-        return 0;
+int main() {
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int length = sizeof(array) / sizeof(array[0]);
+    printf("Các phần tử trong mảng là:\n");
+    for (int i = 0; i < length; i++) {
+        printf("%d ", array[i]);
     }
+    printf("\n");
+    printf("Độ dài của mảng là: %d\n", length);
+
+    return 0;
 }
