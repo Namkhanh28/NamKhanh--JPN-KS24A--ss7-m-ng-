3d54fbd3f74082bbfa61bf40166caf9450e6a66e
Khánh28- <nguyendanhnamkhanh06@gmail.com>
2024-12-09 01:42:37 +0700

    Add files via upload

---
+++ bai2ss7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int array[5];
+    printf("Nhap 5 so nguyen vao mang\n");
+    for(int i=0 ;i<=5 ;i++){
+        printf("so nguyen do la %d,"i+1)
+        scanf("%d";&array[i]);
+    }
+    printf("phan tu trong mang ay la\n");
+    for(int i=0 ;i<=5 ;i++){
+        printf("%d\n",array[i]);
+    }
+    return 0;
+}
